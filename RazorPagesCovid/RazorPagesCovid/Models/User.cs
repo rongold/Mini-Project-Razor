3b52d98d791437d0719ba5a482ca993e485cae46
atchu98 <atchuthan98@googlemail.com>
2021-09-01 11:54:54 +0100

    Model Validation

--- RazorPagesCovid/RazorPagesCovid/Models/User.cs
+++ RazorPagesCovid/RazorPagesCovid/Models/User.cs
@@ -14,23 +14,35 @@ namespace RazorPagesCovid.Models
 		[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
 		[Display(Name = "First Name")]
 		[StringLength(30)]
+		[Required]
 		public string FirstName { get; set; }
 
 		[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
 		[Display(Name = "Last Name")]
 		[StringLength(30)]
+		[Required]
 		public string LastName { get; set; }
 
-		[Range(16, 99)]
+		[Range(1, 99)]
+		[Required]
 		public int Age { get; set; }
-
+		[Range(1, 9999)]
+		[Required]
+		[Display(Name = "House Number")]
 		public string HouseNumber { get; set; }
+		[RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+		[Required]
+		[StringLength(100)]
+		[Display(Name = "Street Name")]
 		public string StreetName { get; set; }
 
 		[DataType(DataType.PostalCode)]
+		[Required]
 		public string PostCode { get; set; }
 
 		[DataType(DataType.PhoneNumber)]
+		[Required]
+		[Display(Name = "Phone Number")]
 		public int PhoneNumber { get; set; }
 
 
