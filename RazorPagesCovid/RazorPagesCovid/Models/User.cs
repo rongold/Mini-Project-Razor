488da270bacf497db7d214d41fcc2f8a542b682c
atchu98 <atchuthan98@googlemail.com>
2021-09-01 14:46:27 +0100

    xy

--- RazorPagesCovid/RazorPagesCovid/Models/User.cs
+++ RazorPagesCovid/RazorPagesCovid/Models/User.cs
@@ -13,13 +13,13 @@ namespace RazorPagesCovid.Models
 
 		[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
 		[Display(Name = "First Name")]
-		[StringLength(30)]
+		[StringLength(30, MinimumLength = 3)]
 		[Required]
 		public string FirstName { get; set; }
 
 		[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
 		[Display(Name = "Last Name")]
-		[StringLength(30)]
+		[StringLength(30, MinimumLength = 3)]
 		[Required]
 		public string LastName { get; set; }
 
@@ -32,17 +32,19 @@ namespace RazorPagesCovid.Models
 		public string HouseNumber { get; set; }
 		[RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
 		[Required]
-		[StringLength(100)]
+		[StringLength(100, MinimumLength = 3)]
 		[Display(Name = "Street Name")]
 		public string StreetName { get; set; }
 
 		[DataType(DataType.PostalCode)]
 		[Required]
+		[RegularExpression(@"^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$")]
 		public string PostCode { get; set; }
-
+		
 		[DataType(DataType.PhoneNumber)]
 		[Required]
 		[Display(Name = "Phone Number")]
+		[RegularExpression(@"\(?\d{4}\)?-? *\d{3}-? *-?\d{4}|^\+(?:[0-9] ?){6,14}[0-9]$")]
 		public int PhoneNumber { get; set; }
 
 
