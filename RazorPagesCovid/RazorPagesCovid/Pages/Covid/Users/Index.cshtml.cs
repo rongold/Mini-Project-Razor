5a4a1ae2b52c1039a2d99335e920ba43935d878d
umz-eats-doner-kebab <Umar.T.Majid@gmail.com>
2021-09-01 11:51:05 +0100

    Created Search Bar that finds the people vaccine based on the input of both the first name and last name.

--- RazorPagesCovid/RazorPagesCovid/Pages/Covid/Users/Index.cshtml.cs
+++ RazorPagesCovid/RazorPagesCovid/Pages/Covid/Users/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesCovid.Data;
 using RazorPagesCovid.Models;
@@ -20,10 +21,23 @@ namespace RazorPagesCovid.Pages.Covid.Users
         }
 
         public IList<User> User { get;set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        
+        public SelectList GetUserName { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string UserName { get; set; }
 
         public async Task OnGetAsync()
         {
-            User = await _context.User.ToListAsync();
+            var getPeopleNames = from u in _context.User
+                         select u;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                getPeopleNames = getPeopleNames.Where(s => s.FirstName.Contains(SearchString) || s.LastName.Contains(SearchString));
+            }
+            User = await getPeopleNames.ToListAsync();
         }
     }
 }
