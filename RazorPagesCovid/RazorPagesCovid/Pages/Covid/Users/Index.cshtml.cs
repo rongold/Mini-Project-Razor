cc81d1d55d1e85ab6d11ca3f84a115f4e065413c
umz-eats-doner-kebab <Umar.T.Majid@gmail.com>
2021-09-01 13:52:57 +0100

    Added search bar

--- RazorPagesCovid/RazorPagesCovid/Pages/Covid/Users/Index.cshtml.cs
+++ RazorPagesCovid/RazorPagesCovid/Pages/Covid/Users/Index.cshtml.cs
@@ -21,22 +21,53 @@ namespace RazorPagesCovid.Pages.Covid.Users
         }
 
         public IList<User> User { get;set; }
+
         
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
         
         public SelectList GetUserName { get; set; }
+       
+        
         [BindProperty(SupportsGet = true)]
-        public string UserName { get; set; }
+        public string VaccineName { get; set; }
 
         public async Task OnGetAsync()
         {
+            /*var getPeopleNames = from a in _context.Apppointment
+                                 join u in _context.User
+                                 on a.UserId equals u.UserId
+                                 //join v in _context.Vaccine
+                                 //on a.VaccineId equals v.VaccineId
+                                 //orderby v.VaccineName
+                                 select new
+                                 { 
+                                    u.FirstName,
+                                    u.LastName,
+                                    u.Age,
+                                    u.HouseNumber,
+                                    u.PostCode,
+                                    u.StreetName,
+                                    a.DateOfAppointment
+                                 };*/
+
+
             var getPeopleNames = from u in _context.User
-                         select u;
+                                 select u;
+
             if (!string.IsNullOrEmpty(SearchString))
             {
                 getPeopleNames = getPeopleNames.Where(s => s.FirstName.Contains(SearchString) || s.LastName.Contains(SearchString));
             }
+
+            /*
+            if (!string.IsNullOrEmpty(VaccineName))
+            {
+                VaccineName = getPeopleNames.Where(x => x.VaccineName == VaccineName);
+            }
+            */
+
+            //GetUserName = new SelectList(await genreQuery.Distinct().ToListAsync());
             User = await getPeopleNames.ToListAsync();
         }
     }
