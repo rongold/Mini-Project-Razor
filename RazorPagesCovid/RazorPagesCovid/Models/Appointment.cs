b5238dc7bde84a6da0af5aac6467bada1bcfa677
atchu98 <atchuthan98@googlemail.com>
2021-09-02 12:41:31 +0100

    update css

--- RazorPagesCovid/RazorPagesCovid/Models/Appointment.cs
+++ RazorPagesCovid/RazorPagesCovid/Models/Appointment.cs
@@ -9,21 +9,22 @@ namespace RazorPagesCovid.Models
         [Key]
         public int AppointmentId { get; set; }
 
-        [Required,StringLength(1000, MinimumLength = 1),RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Requires a Capital letter at the begining and can only contain letters, Minimum Length is 1")]
+        [Required, StringLength(1000, MinimumLength = 1), RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Requires a Capital letter at the begining and can only contain letters, Minimum Length is 1")]
         public string Location { get; set; }
 
         [Display(Name = "Date of Appointment"), DataType(DataType.Date)]
         public DateTime DateOfAppointment { get; set; }
-        
+
         [Display(Name = "Vaccine"), Required]
         public int VaccineId { get; set; }
 
         [ForeignKey("VaccineId")]
         public virtual Vaccine Vaccine { get; set; }
-        
+
         [Display(Name = "User ID"), Required]
         public int UserId { get; set; }
 
+        [Display(Name = "User")]
         public virtual User user { get; set; }
 
     }
